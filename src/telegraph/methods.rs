9617d346a6b8026ebc76f0bf1962ae6c11dc7e6b
Filipponik <mitin.maks@bk.ru>
2024-07-20 17:06:20 +0300

    feat(telegraph)!: Created contracts for api methods for page

--- src/telegraph/methods.rs
+++ src/telegraph/methods.rs
@@ -1,5 +1,13 @@
+use crate::telegraph::types::NodeElement;
+
 mod account;
 mod page;
+
+pub enum FieldToChange {
+    ShortName,
+    AuthorName,
+    AuthorUrl,
+}
 
 pub async fn create_account(
     short_name: &str,
@@ -7,12 +15,6 @@ pub async fn create_account(
     author_url: Option<&str>
 ) {
     account::create(short_name, author_name, author_url).await
-}
-
-pub enum FieldToChange {
-    ShortName,
-    AuthorName,
-    AuthorUrl,
 }
 
 pub async fn edit_account_info(
@@ -36,4 +38,52 @@ pub async fn revoke_access_token(
     access_token: &str,
 ) {
     account::revoke_token(access_token).await
+}
+
+pub async fn create_page(
+    access_token: &str,
+    title: &str,
+    author_name: Option<&str>,
+    author_url: Option<&str>,
+    content: &Vec<NodeElement>,
+    return_content: bool,
+) {
+    page::create(access_token, title, author_name, author_url, content, return_content).await
+}
+
+pub async fn edit_page(
+    access_token: &str,
+    path: &str,
+    title: &str,
+    author_name: Option<&str>,
+    author_url: Option<&str>,
+    content: &Vec<NodeElement>,
+    return_content: bool,
+) {
+    page::edit(access_token, path, title, author_name, author_url, content, return_content).await
+}
+
+pub async fn get_page(
+    path: &str,
+    return_content: bool,
+) {
+    page::get(path, return_content).await
+}
+
+pub async fn get_page_list(
+    access_token: &str,
+    offset: u64,
+    limit: u8,
+) {
+    page::get_list(access_token, offset, limit).await
+}
+
+pub async fn get_views(
+    path: &str,
+    year: Option<u16>,
+    month: Option<u8>,
+    day: Option<u8>,
+    hour: Option<u8>,
+) {
+    page::get_views(path, year, month, day, hour).await
 }
---
+++ src/telegraph/methods/page.rs
@@ -0,0 +1,49 @@
+use crate::telegraph::types::NodeElement;
+
+pub async fn create(
+    access_token: &str,
+    title: &str,
+    author_name: Option<&str>,
+    author_url: Option<&str>,
+    content: &Vec<NodeElement>,
+    return_content: bool,
+) {
+    todo!()
+}
+
+pub async fn edit(
+    access_token: &str,
+    path: &str,
+    title: &str,
+    author_name: Option<&str>,
+    author_url: Option<&str>,
+    content: &Vec<NodeElement>,
+    return_content: bool,
+) {
+    todo!()
+}
+
+pub async fn get(
+    path: &str,
+    return_content: bool,
+) {
+    todo!()
+}
+
+pub async fn get_list(
+    access_token: &str,
+    offset: u64,
+    limit: u8,
+) {
+    todo!()
+}
+
+pub async fn get_views(
+    path: &str,
+    year: Option<u16>,
+    month: Option<u8>,
+    day: Option<u8>,
+    hour: Option<u8>,
+) {
+    todo!()
+}
