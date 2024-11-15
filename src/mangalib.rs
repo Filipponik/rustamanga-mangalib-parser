c94817c6e2ea49025326aed861e3471d74f8d7e4
mma <mma@pst.net>
2024-11-15 19:10:48 +0300

    feat(mangalib): Added better error handling, now without panics

--- src/mangalib.rs
+++ src/mangalib.rs
@@ -3,13 +3,25 @@
 use headless_chrome::Browser;
 use serde::{Deserialize, Deserializer, Serialize};
 use serde_json::Value;
-use std::error::Error;
-use tracing::debug;
+use tracing::{debug, error};
 
 const URL: &str = "https://mangalib.me";
 const USER_AGENT: &str = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.114 Safari/537.36";
 const ACCEPT_LANGUAGE: &str = "en-US,en;q=0.9,hi;q=0.8,es;q=0.7,lt;q=0.6";
 const PLATFORM: &str = "macOS";
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum MangalibError {
+    SerdeParseError,
+    BrowserCreateError,
+    BrowserTabCreateError,
+    BrowserNavigateError,
+    SetUserAgentError,
+    BrowserWaitNavigateTooLong,
+    BrowserWaitElementTooLong,
+    BrowserFunctionError,
+    BrowserGetContentError,
+}
 
 fn get_url() -> String {
     URL.to_string()
@@ -18,30 +30,44 @@ fn get_url() -> String {
 pub async fn get_manga_chapter_images(
     slug: &str,
     manga_chapter: &MangaChapter,
-) -> Result<Vec<String>, Box<dyn Error>> {
-    let browser = Browser::default()?;
-    let tab = browser.new_tab()?;
+) -> Result<Vec<String>, MangalibError> {
+    let browser = Browser::default().map_err(|_| MangalibError::BrowserCreateError)?;
+    let tab = browser.new_tab().map_err(|_| MangalibError::BrowserTabCreateError)?;
     let web_url = format!(
         "{}/{slug}/v{}/c{}?page=1",
         get_url(),
         manga_chapter.chapter_volume,
         manga_chapter.chapter_number
     );
+
     debug!("Searching manga chapters at {web_url}");
-    tab.set_user_agent(USER_AGENT, Some(ACCEPT_LANGUAGE), Some(PLATFORM))?;
-    tab.navigate_to(&web_url)?.wait_until_navigated()?;
-    let reader_element = tab.wait_for_element(".reader-view")?;
+    tab.set_user_agent(USER_AGENT, Some(ACCEPT_LANGUAGE), Some(PLATFORM)).map_err(|_| MangalibError::SetUserAgentError)?;
+    tab.navigate_to(&web_url)
+        .map_err(|_| MangalibError::BrowserNavigateError)?
+        .wait_until_navigated()
+        .map_err(|_| MangalibError::BrowserWaitNavigateTooLong)?;
+    let reader_element = tab.wait_for_element(".reader-view")
+        .map_err(|_| MangalibError::BrowserWaitElementTooLong)?;
+
     let js_obj = reader_element.call_js_fn(r#"
         function f() {
             return JSON.stringify(window.__pg.map(el => el.u).map(image => window.__info.servers[window.__info.img.server]+window.__info.img.url+image));
         }
-    "#, vec![], false)?;
+    "#, vec![], false)
+        .map_err(|_| MangalibError::BrowserFunctionError)?;
 
-    let res: Vec<String> = match js_obj.value.unwrap() {
-        Value::String(v) => serde_json::from_str(&v)?,
-        _ => panic!("shit happens!"),
+    let val = match js_obj.value {
+        Some(v) => v,
+        _ => return Err(MangalibError::BrowserFunctionError),
     };
+
+    let res: Vec<String> = match val {
+        Value::String(v) => serde_json::from_str(&v).map_err(|_| MangalibError::SerdeParseError)?,
+        _ => return Err(MangalibError::BrowserFunctionError),
+    };
+
     let _ = tab.close_target();
+
     Ok(res)
 }
 
@@ -54,23 +80,26 @@ pub struct MangaPreview {
     image_url: String,
 }
 
-pub async fn search_manga(search_input: &str) -> Result<Vec<MangaPreview>, Box<dyn Error>> {
-    let web_url = &format!(
+pub async fn search_manga(search_input: &str) -> Result<Vec<MangaPreview>, MangalibError> {
+    let web_url = format!(
         "{}/manga-list?sort=rate&dir=desc&page=1&name={search_input}",
         get_url()
     );
-    let browser = Browser::default().unwrap();
-    let tab = browser.new_tab().unwrap();
+    let browser = Browser::default().map_err(|_| MangalibError::BrowserCreateError)?;
+    let tab = browser.new_tab().map_err(|_| MangalibError::BrowserTabCreateError)?;
     debug!("Searching manga at {web_url}");
-    tab.set_user_agent(USER_AGENT, Some(ACCEPT_LANGUAGE), Some(PLATFORM))
-        .unwrap();
-    tab.navigate_to(web_url)
-        .unwrap()
+
+    tab.set_user_agent(USER_AGENT, Some(ACCEPT_LANGUAGE), Some(PLATFORM)).map_err(|_| MangalibError::SetUserAgentError)?;
+    tab.navigate_to(&web_url)
+        .map_err(|_| MangalibError::BrowserNavigateError)?
         .wait_until_navigated()
-        .unwrap();
+        .map_err(|_| MangalibError::BrowserWaitNavigateTooLong)?;
 
     let start = std::time::Instant::now();
-    tab.wait_for_element(".media-card").unwrap();
+
+    tab.wait_for_element(".media-card")
+        .map_err(|_| MangalibError::BrowserWaitElementTooLong)?;
+
     let func_result = tab.evaluate(
         r#"
         JSON.stringify(Array.from(document.querySelectorAll('.media-card')).map(function (el) {
@@ -85,11 +114,18 @@ pub async fn search_manga(search_input: &str) -> Result<Vec<MangaPreview>, Box<d
         }))
     "#,
         false,
-    )?;
+    )
+        .map_err(|_| MangalibError::BrowserFunctionError)?;
 
-    Ok(match func_result.value.unwrap() {
-        Value::String(v) => serde_json::from_str(&v).unwrap(),
-        _ => panic!("shit happens!"),
+
+    let val = match func_result.value {
+        Some(v) => v,
+        _ => return Err(MangalibError::BrowserFunctionError),
+    };
+
+    Ok(match val {
+        Value::String(v) => serde_json::from_str(&v).map_err(|_| MangalibError::SerdeParseError)?,
+        _ => return Err(MangalibError::BrowserFunctionError),
     })
 }
 
@@ -110,24 +146,33 @@ impl MangaChapter {
     }
 }
 
-pub async fn get_manga_chapters(slug: &str) -> Result<Vec<MangaChapter>, Box<dyn Error>> {
+pub async fn get_manga_chapters(slug: &str) -> Result<Vec<MangaChapter>, MangalibError> {
     let web_url = &format!("{}/{slug}?section=chapters", get_url());
-    let browser = Browser::default().unwrap();
-    let tab = browser.new_tab().unwrap();
+    let browser = Browser::default().map_err(|_| MangalibError::BrowserCreateError)?;
+    let tab = browser.new_tab().map_err(|_| MangalibError::BrowserTabCreateError)?;
     debug!("Searching manga chapters at {web_url}");
-    tab.set_user_agent(USER_AGENT, Some(ACCEPT_LANGUAGE), Some(PLATFORM))
-        .unwrap();
-    tab.navigate_to(web_url)
-        .unwrap()
+
+    tab.set_user_agent(USER_AGENT, Some(ACCEPT_LANGUAGE), Some(PLATFORM)).map_err(|_| MangalibError::SetUserAgentError)?;
+    tab.navigate_to(&web_url)
+        .map_err(|_| MangalibError::BrowserNavigateError)?
         .wait_until_navigated()
-        .unwrap();
+        .map_err(|_| MangalibError::BrowserWaitNavigateTooLong)?;
+
     let elem = tab.wait_for_element(".media-chapter__name.text-truncate a");
     let elem = match elem {
         Ok(value) => value,
         Err(err) => {
-            panic!("{}\n{}", err, tab.get_content().unwrap())
+            let content = match tab.get_content().map_err(|_| MangalibError::BrowserGetContentError) {
+                Ok(val) => val,
+                Err(err) => return Err(err),
+            };
+
+            error!("Error waiting element too long {}\n{}", err, content);
+
+            return Err(MangalibError::BrowserWaitElementTooLong);
         }
     };
+
     let js_obj = elem.call_js_fn(
         r#"
         function f() {
@@ -136,10 +181,15 @@ pub async fn get_manga_chapters(slug: &str) -> Result<Vec<MangaChapter>, Box<dyn
     "#,
         vec![],
         false,
-    )?;
+    ).map_err(|_| MangalibError::BrowserFunctionError)?;
 
-    Ok(match js_obj.value.unwrap() {
-        Value::String(v) => serde_json::from_str(&v).unwrap(),
+    let val = match js_obj.value {
+        Some(v) => v,
+        _ => return Err(MangalibError::BrowserFunctionError),
+    };
+    
+    Ok(match val {
+        Value::String(v) => serde_json::from_str(&v).map_err(|_| MangalibError::SerdeParseError)?,
         _ => panic!("shit happens!"),
     })
 }
