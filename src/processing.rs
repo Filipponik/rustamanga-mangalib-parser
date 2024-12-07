5bf9e15500d50f098c899e945f412c852ab556ef
Filipponik <mitin.maks@bk.ru>
2024-12-07 20:05:12 +0300

    feat(processing): Separated processing from server module

---
+++ src/processing.rs
@@ -0,0 +1,112 @@
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
+use serde::{Deserialize, Serialize};
+use tokio::sync::Semaphore;
+use tracing::{error, info};
+use crate::mangalib;
+
+macro_rules! retry {
+    ($f:expr, $count:expr) => {{
+        let mut tries = 0;
+        let result = loop {
+            let result = $f;
+            tries += 1;
+            if result.is_ok() || tries >= $count {
+                break result;
+            }
+        };
+        result
+    }};
+    ($f:expr) => {
+        retry!($f, 5)
+    };
+}
+
+#[derive(Deserialize)]
+pub struct ScrapMangaRequest {
+    slug: String,
+    callback_url: String,
+}
+
+pub async fn process(chrome_max_count: u16, payload: ScrapMangaRequest) {
+    let manga = get_manga_urls(&payload.slug, chrome_max_count).await;
+    info!("Sending manga to {}", payload.callback_url);
+    let response = send_info_about_manga(&payload.callback_url, &manga).await;
+    match response {
+        Ok(body) => info!("Successfully sent manga: {body}"),
+        Err(err) => error!("Error while sending manga: {err:?}"),
+    }
+}
+
+pub async fn get_manga_urls(slug: &str, chrome_max_count: u16) -> PublishedManga {
+    let chapter_urls_map: Arc<Mutex<HashMap<mangalib::MangaChapter, Vec<String>>>> =
+        Arc::new(Mutex::new(HashMap::new()));
+    let mut chapters = mangalib::get_manga_chapters(slug).await.unwrap();
+    chapters.reverse();
+    let mut threads = vec![];
+    let semaphore = Arc::new(Semaphore::new(chrome_max_count as usize));
+    for chapter in chapters.clone() {
+        let urls = Arc::clone(&chapter_urls_map);
+        let slug = slug.to_string();
+        let semaphore = semaphore.clone();
+        let thread = tokio::spawn(async move {
+            let _permit = semaphore.acquire().await.unwrap();
+            let result = retry!(mangalib::get_manga_chapter_images(&slug, &chapter).await).unwrap();
+            let mut urls = urls.lock().unwrap();
+            urls.insert(chapter.clone(), result);
+        });
+
+        threads.push(thread);
+    }
+
+    futures::future::join_all(threads).await;
+    let chapter_urls_map = chapter_urls_map.lock().unwrap().clone();
+
+    publish_manga(slug, &chapters, &chapter_urls_map).await
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PublishedManga {
+    pub slug: String,
+    pub chapters: Vec<PublishedMangaChapter>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct PublishedMangaChapter {
+    pub url: Option<String>,
+    pub chapter: String,
+    pub volume: String,
+    pub images_urls: Vec<String>,
+}
+
+async fn publish_manga(
+    slug: &str,
+    chapters: &[mangalib::MangaChapter],
+    chapter_urls_map: &HashMap<mangalib::MangaChapter, Vec<String>>,
+) -> PublishedManga {
+    let mut telegraph_urls: Vec<PublishedMangaChapter> = vec![];
+    for chapter in chapters {
+        let url_images = chapter_urls_map.get(chapter).unwrap();
+        telegraph_urls.push(PublishedMangaChapter {
+            url: None,
+            chapter: chapter.chapter_number.clone(),
+            volume: chapter.chapter_volume.clone(),
+            images_urls: url_images.clone(),
+        });
+    }
+
+    PublishedManga {
+        slug: slug.to_string(),
+        chapters: telegraph_urls,
+    }
+}
+
+async fn send_info_about_manga(url: &str, manga: &PublishedManga) -> reqwest::Result<String> {
+    reqwest::Client::new()
+        .post(url)
+        .json(manga)
+        .send()
+        .await?
+        .text()
+        .await
+}
