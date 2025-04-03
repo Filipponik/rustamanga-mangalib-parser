39046045281e68c90edde68ffaf7a2e1f99ee734
mma <mma@pst.net>
2025-04-03 18:33:49 +0300

    feat: Change send_resource for sync sending

--- src/send_resource.rs
+++ src/send_resource.rs
@@ -1,7 +1,6 @@
 use crate::mangalib::MangaPreview;
 use reqwest::Client;
 use thiserror::Error;
-use tokio::task::JoinHandle;
 use tracing::{error, info};
 
 const MANGALIB_STATIC_RESOURCE: &str = include_str!("../resource/json/mangalib_manga_list.json");
@@ -15,30 +14,22 @@ pub enum Error {
 }
 
 pub async fn send_resource(url: &str) -> Result<(), Error> {
-    let resource_vec: Vec<MangaPreview> =
+    let mangas: Vec<MangaPreview> =
         serde_json::from_str(MANGALIB_STATIC_RESOURCE).map_err(Error::Parse)?;
 
     let client = Client::new();
-    let handlers = resource_vec
-        .into_iter()
-        .map(|res| process_single_resource(url.to_string(), res, client.clone()));
-
-    futures::future::join_all(handlers).await;
-
+    for manga in mangas {
+        match send_single_resource(client.clone(), url, manga).await {
+            Ok(()) => info!("Successfully sent"),
+            Err(err) => error!("Failed to send resource: {}", err),
+        }
+    }
+    
     Ok(())
 }
 
-fn process_single_resource(url: String, res: MangaPreview, client: Client) -> JoinHandle<()> {
-    tokio::spawn(async move {
-        match send_single_resource(client, &url, res).await {
-            Ok(()) => info!("Successfully sent"),
-            Err(err) => error!("Failed to send resource: {}", err),
-        };
-    })
-}
-
-async fn send_single_resource(client: Client, url: &str, res: MangaPreview) -> Result<(), Error> {
-    client.post(url).json(&res).send().await?;
+async fn send_single_resource(client: Client, url: &str, manga: MangaPreview) -> Result<(), Error> {
+    client.post(url).json(&manga).send().await?;
 
     Ok(())
 }
