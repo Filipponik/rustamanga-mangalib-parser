d763386bb337f7f7eb045301880f992dc35f231b
mma <mma@pst.net>
2026-08-28 14:47:17 +0300

    fix: Mark get_user_list as not implemented instead of todo

--- src/processing/user_list.rs
+++ src/processing/user_list.rs
@@ -9,6 +9,13 @@ pub struct GetUserListParams {
 #[derive(Debug, thiserror::Error)]
 pub enum Error {}
 
+/// Extension point for the `get_user_list` command, the feature itself is not implemented yet.
+///
+/// # Errors
+/// Never returns an error while the feature is not implemented.
+///
+/// # Panics
+/// Always panics with `unimplemented!` until the feature is implemented.
 pub async fn handle(params: &GetUserListParams) -> Result<(), Error> {
-    todo!();
+    unimplemented!("get_user_list command is not implemented yet");
 }
